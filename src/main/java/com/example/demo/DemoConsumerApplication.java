ff2bb75441fc6678ef997f9c75c950b861233fdb
sharifaspak <37010522+sharifaspak@users.noreply.github.com>
2021-06-12 12:57:52 +0400

    first commit to new repo

--- src/main/java/com/example/demo/DemoConsumerApplication.java
+++ src/main/java/com/example/demo/DemoConsumerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoConsumerApplication {
 
 	public static void main(String[] args) {
+		//first commit to demo
 		SpringApplication.run(DemoConsumerApplication.class, args);
 	}
 
